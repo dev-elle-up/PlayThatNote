35491ae0aaa0b92f42f823fa1e348488c23df0a8
Mello-Cello <celloelle@gmail.com>
2019-07-11 18:57:04 -0700

    Implemented pitch detection, audio buffer, and PitchFinder library.

--- src/components/Analyzer.js
+++ src/components/Analyzer.js
@@ -1,6 +1,30 @@
-const Pitchfinder = require("pitchfinder");
-const detectPitch = Pitchfinder.AMDF();
+import React, { Component } from 'react';
+// import a component if you want to return it in your render
+import Pitchfinder from 'pitchfinder';
 
-const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
-const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
-const pitch = detectPitch(float32Array); // null if pitch cannot be identified
+class Analyzer extends Component {
+
+  detectPitch = Pitchfinder.YIN();
+
+  // myAudioBuffer = this.props.userAudioFromMic.getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
+  // float32Array = this.myAudioBuffer.getChannelData(0); // get a single channel of sound
+  // pitch = this.detectPitch(float32Array); // null if pitch cannot be identified
+
+// take mic input and .connect to audioBuffer?
+
+  render(){
+    return(<div></div>);
+  }
+
+}
+
+export default Analyzer;
+
+
+
+// const Pitchfinder = require("pitchfinder");
+// const detectPitch = Pitchfinder.AMDF();
+//
+// const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
+// const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
+// const pitch = detectPitch(float32Array); // null if pitch cannot be identified
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Analyzer from './Analyzer.js';
+import Pitchfinder from 'pitchfinder';
 
 class Game extends Component {
   constructor(props){
     super(props);
     this.state={
       userAudioFromMic: null,
+      processor: null,
       // last_propmted_note: null,
       // current_prompted_note: null,
       // current_user_note: null,
@@ -18,9 +21,7 @@ class Game extends Component {
   }
 
   componentWillUnmount() {
-    this.state.userAudioFromMic.getTracks().forEach(track => track.stop());
-    this.setState({ userAudioFromMic: null });
-    // this.setState({userAudioFromMic: null})
+    this.state.processor.disconnect();
   }
 
   async getMicrophoneFeed() {
@@ -30,12 +31,35 @@ class Game extends Component {
     });
     this.setState({userAudioFromMic})
      // userAudioFromMic is now accessible in state as MediaStream
+
   }
 
 
   showMeTheState = () => {
     console.log('in showMeTheState, userAudioFromMic: ', this.state.userAudioFromMic);
+    console.log('in showMeTheState, userAudioFromMic type: ', typeof this.state.userAudioFromMic);
+    const context = new AudioContext();
+    const source = context.createMediaStreamSource(this.state.userAudioFromMic);
+    const processor = context.createScriptProcessor(1024, 1, 1);
+    this.setState({processor: processor});
+    source.connect(processor);
+    processor.connect(context.destination);
+     console.log('processor', processor);
+    const detectPitch = Pitchfinder.AMDF();
+
+    processor.onaudioprocess = function(audioBuffer) {
+      // console.log(audioBuffer);
+      // const float32Array = audioBuffer.getChannelData(0);
+      const pitch = detectPitch(audioBuffer.inputBuffer.getChannelData(0));
+      console.log('pitch: ', pitch);
+
+
+    }
   }
+  // const detectPitch = Pitchfinder.AMDF();
+  // const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
+  // const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
+  // const pitch = detectPitch(float32Array); // null if pitch cannot be identified
 
   render() {
 
@@ -46,6 +70,8 @@ class Game extends Component {
         <button onClick={this.skipNote}>skip</button>
         <button onClick={this.props.finishGameCallback}>finished</button>
         <button onClick={this.showMeTheState}>show me the state</button>
+        <Analyzer
+        mediaStream={this.state.userAudioFromMic}/>
       </section>
     );
   }
